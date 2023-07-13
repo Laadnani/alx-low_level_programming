73a3f58c83a925c8891d50ace11ff3692b8c38b7
Laadnani Mohamed <laadnani.med@gmail.com>
2023-07-13 08:07:41 -0700

    6-size.c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: returning 0 when successfull
+ */
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int e;
+	float f;
+
+	printf("Size of a char : %c", sizeof(a));
+	printf("Size of a int : %d", sizeof(a));
+	printf("Size of a char : %d", sizeof(a));
+	printf("Size of a char : %d", sizeof(a));
+	printf("Size of a char : %f", sizeof(a));
+
+	return (0);
+}
