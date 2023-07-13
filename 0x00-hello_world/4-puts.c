077be8dbf9e6f62a65979d099fc6d36de9fd3c74
Laadnani Mohamed <laadnani.med@gmail.com>
2023-07-13 07:32:26 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,13 @@
 #include <stdio.h>
+/**
+ * main - no arguments needed
+ *
+ * @char : line to be printed
+ *
+ * puts functions taking the sting line as parameter
+ *
+ * return : always 0 uppon success
+ */
 int main(void)
 {
 	char line[] = "\"Programming is like building a multilingual puzzle";
