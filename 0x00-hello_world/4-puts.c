ad7ccbe9f9b6bf9cfb84a1895cb045f5726cc764
Laadnani Mohamed <laadnani.med@gmail.com>
2023-07-13 07:20:16 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point 
+ *
+ * @line - character array to be printed
+ *
+ * Prints the puts function
+ *
+ * Returns : always 0 (success)
+ */
+int main (void)
+{
+	char line[]="Programming is like building a multilingual puzzle";
+	puts(line);
+	return(0);
+}
