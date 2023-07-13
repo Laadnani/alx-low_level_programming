cea0eddb70284e42d90f1bf3b22599dce419555a
Laadnani Mohamed <laadnani.med@gmail.com>
2023-07-13 08:11:26 -0700

    6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,11 +12,11 @@ int main(void)
 	long long int e;
 	float f;
 
-	printf("Size of a char : %c", sizeof(a));
-	printf("Size of a int : %d", sizeof(a));
-	printf("Size of a char : %d", sizeof(a));
-	printf("Size of a char : %d", sizeof(a));
-	printf("Size of a char : %f", sizeof(a));
+	printf("Size of a char : %c byte(s)\n", sizeof(a));
+	printf("Size of a int : %d byte(s)\n", sizeof(b));
+	printf("Size of a char : %d byte(s)\n", sizeof(c));
+	printf("Size of a char : %d byte(s)\n", sizeof(d));
+	printf("Size of a char : %f byte(s)\n", sizeof(f));
 
 	return (0);
 }
