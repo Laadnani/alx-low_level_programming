f57f99eadb85615c2327e01714f5dbc73f803357
Laadnani Mohamed <laadnani.med@gmail.com>
2023-07-13 09:16:34 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,9 +4,9 @@
  *
  * Return: return 1 when successfull
  */
-int main (void)
+int main(void)
 {
-	char print[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	char print[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	fputs(print, stdout);
 	return (1);
