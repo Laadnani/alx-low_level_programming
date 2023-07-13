2a1d239e82bd7fe7913d50fdd65d5514f3bba0b5
Laadnani Mohamed <laadnani.med@gmail.com>
2023-07-13 07:27:23 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,17 +1,7 @@
 #include <stdio.h>
-
-/**
- * main - Entry point 
- *
- * @line - character array to be printed
- *
- * Prints the puts function
- *
- * Returns : always 0 (success)
- */
-int main(void)
+int main (void)
 {
-	char line[]="\"Programming is like building a multilingual puzzle";
+	char line[] = "\"Programming is like building a multilingual puzzle";
 	puts(line);
 	return(0);
 }
