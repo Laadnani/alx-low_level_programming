57e126179f34f223082d93a4e36cb1dcec18b501
Laadnani Mohamed <laadnani.med@gmail.com>
2023-07-13 07:21:46 -0700

    4-puts.c betty beauty added

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -9,9 +9,9 @@
  *
  * Returns : always 0 (success)
  */
-int main (void)
+int main(void)
 {
-	char line[]="Programming is like building a multilingual puzzle";
+	char line[]="\"Programming is like building a multilingual puzzle";
 	puts(line);
 	return(0);
 }
