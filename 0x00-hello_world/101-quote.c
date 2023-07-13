ed6fa1c00699ed5b6345d5e4a6409f614f959eb2
Laadnani Mohamed <laadnani.med@gmail.com>
2023-07-13 09:13:36 -0700

    and that piece

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: return 1 when successfull
+ */
+int main (void)
+{
+	char print[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+
+	fput(print, stdout);
+	return (1);
+}
