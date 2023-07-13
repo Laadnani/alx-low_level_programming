8e7ac24f37c95352307008dcce4b98a460f1dca2
Laadnani Mohamed <laadnani.med@gmail.com>
2023-07-13 07:58:59 -0700

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,8 +4,8 @@
  *
  * Return: return 0 uppon success
  */
-int main (void)
+int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
