08ef9817fd369902b9942d5b8eb00778463cf5d7
Laadnani Mohamed <laadnani.med@gmail.com>
2023-07-14 06:45:55 -0700

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: success with 0 result
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0;i<=10;i++)
+	{
+		printf(i);
+	}
+	putchar('\n');
+	return (0);
+}
