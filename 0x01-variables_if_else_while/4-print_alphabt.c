f4a400b4dbb4564e259c39f87bdd0c5afcbec767
Laadnani Mohamed <laadnani.med@gmail.com>
2023-07-14 06:36:21 -0700

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio>
+/**
+ * main - entry point
+ *
+ * Return: 0 when success
+ */
+int main(void)
+{
+	int i = 97;
+
+	while (i <= 122)
+	{
+		for (i == 101 || i == 113)
+		{
+			continue;
+			i++;
+		}
+		putchar(i);
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
