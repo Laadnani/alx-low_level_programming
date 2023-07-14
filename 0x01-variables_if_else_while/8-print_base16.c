c04b6748831866e84661b41b550385a556a7cdb2
Laadnani Mohamed <laadnani.med@gmail.com>
2023-07-14 07:15:21 -0700

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 when success
+ */
+int main(void)
+{
+	int i;
+	int n;
+
+	for (i = 48; i <= 57; i++)
+	{
+		putchar(i);
+	}
+	for (n = 97; i <=102 ;n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+	return (0);
+}
