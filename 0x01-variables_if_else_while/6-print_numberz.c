ef7ca8b7ce6402a55e977701a6f0b5f2b87912d3
Laadnani Mohamed <laadnani.med@gmail.com>
2023-07-14 06:55:08 -0700

    6-print_numberz.c

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0 when success
+ */
+int main(void)
+{
+	int i = '1';
+
+	for (i = '1'; i < 10; i++)
+	{
+		putchar(i);
+	}
+	putchar('\n');
+	return (0);
+}
