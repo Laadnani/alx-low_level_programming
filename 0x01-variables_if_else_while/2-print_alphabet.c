c88ddeb5cd954b35b697b7e32dabcfe4770a0a4b
Laadnani Mohamed <laadnani.med@gmail.com>
2023-07-14 05:38:40 -0700

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: output 0 when successful
+ */
+int main (void) {
+
+	char az[] = [a..z];
+	for ( i = 0; i <= 26; i++)
+		putchar(az[i]);
+	return (0);
+}
