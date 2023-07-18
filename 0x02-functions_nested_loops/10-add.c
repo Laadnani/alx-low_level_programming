9393b37a92062ddb33430693e089069e9acd8dd6
Laadnani Mohamed <laadnani.med@gmail.com>
2023-07-18 09:18:40 -0700

    10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,17 @@
+#include "main"
+/**
+ * add - entry point
+ *
+ * @a: first int to be added
+ *
+ * @b: second int to be added
+ *
+ * Return: returning th sum of a and b
+ */
+int add(int a, int b)
+{
+	int a,b,sum;
+
+	sum = a + b;
+	return(sum);
+}
