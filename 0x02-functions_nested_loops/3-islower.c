e9776bfbe307e6981d3c996e9ab2e738cc9d395b
Laadnani Mohamed <laadnani.med@gmail.com>
2023-07-18 05:38:22 -0700

    3-islower.c

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _islower -entry point
+ *
+ * Return: 1 when lowercase and 0 if not
+ */
+int _islower(char c)
+{
+	if (c >= 97 && c <= 122)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}
