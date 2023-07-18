770d2ce263a1b24c35a4cecccc9cedb4e4912089
Laadnani Mohamed <laadnani.med@gmail.com>
2023-07-18 10:45:05 -0700

    100-times_table.c

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -5,7 +5,7 @@
  * @n: times to print the tables
  */
 void print_times_table(int n)
-{
+{	
 	int i, j, k;
 
 	if (n >= 0 && n <= 15)
@@ -32,7 +32,6 @@ void print_times_table(int n)
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar((k / 100) +'0');
 					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
 				}
@@ -41,7 +40,7 @@ void print_times_table(int n)
 					_putchar(',');
 					_putchar(' ');
 					_putchar((k / 100) + '0');
-					_putchar((k / 10) + '0');
+					_putchar(((k / 10) % 10) + '0');
 					_putchar((k % 10) + '0');
 				}
 			}
