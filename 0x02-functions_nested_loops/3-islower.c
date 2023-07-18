fcec5be670a11a2c48ae7473a6c80da06353cb29
Laadnani Mohamed <laadnani.med@gmail.com>
2023-07-18 05:44:38 -0700

    int _islower(char c) {         if (c >= 97 && c <= 122)         {                 return (1);         }         else         {                 return (0);         } };

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -6,12 +6,12 @@
  */
 int _islower(char c)
 {
-	if (c >= 97 && c <= 122)
-	{
-		return (1);
-	}
-	else
-	{
-		return (0);
-	}
+        if (c >= 97 && c <= 122)
+        {
+                return (1);
+        }
+        else
+        {
+                return (0);
+        }
 }
