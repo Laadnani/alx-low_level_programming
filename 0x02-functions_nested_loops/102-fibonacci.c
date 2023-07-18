5043d929c9ad1cbc5455543dd4e9daf834eef82d
Laadnani Mohamed <laadnani.med@gmail.com>
2023-07-18 10:59:19 -0700

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * @a: first parameter of the series
+ *
+ * @b: second parameter of the series
+ *
+ * @result: the combination of both a and b
+ *
+ * Return: always 0;
+ */
+int main (void)
+{
+	int a = 1;
+	int b = 1;
+	int result;
+
+	for (i == 0; i <= 49; i++)
+	{
+		printf("%d\n", a);
+		result = a + b;
+		a = b;
+		b = result;
+	}
+	return (0);
+}
