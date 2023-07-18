48e7a992cd70d61a59b6016e48e2f1d1ec06eaa0
Laadnani Mohamed <laadnani.med@gmail.com>
2023-07-18 05:01:58 -0700

    2-print_alphabet_x10.c

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - entry point
+ *
+ * Return: no return on Void
+ */
+void print_alphabet_x10(void)
+{
+	int i;
+	char a;
+
+	while (i = 0; i <= 9; i++)
+	{
+		for (a = 'a'; a <= 'z'; a++)
+		{
+			_putchar(a);
+		}
+		_putchar('\n');
+	}	
+}
