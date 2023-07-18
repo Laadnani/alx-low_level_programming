9507f1b37aa4b78edd9840def62aed30e3159c0d
Laadnani Mohamed <laadnani.med@gmail.com>
2023-07-18 02:17:54 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: returning 0 when successfull
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,3 @@
+0x02 C programming tasks
+
+going throught functions and arguments used in the C language
