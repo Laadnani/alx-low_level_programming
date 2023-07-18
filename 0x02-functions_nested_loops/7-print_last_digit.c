3a3d227c6a76574c4a06307d3f488c8a0ed416f1
Laadnani Mohamed <laadnani.med@gmail.com>
2023-07-18 07:14:11 -0700

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - entry point
+ *
+ * @num: number to print the last digit of
+ *
+ * Return: value of the last digit of num
+ */
+int print_last_digit(int num)
+{
+	r = num % 10;
+	return (r);
+}
