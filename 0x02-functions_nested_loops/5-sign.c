b379c5d6aaad2d71f91ff1f82e1f7a7011f84a3f
Laadnani Mohamed <laadnani.med@gmail.com>
2023-07-18 06:55:50 -0700

    5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,21 +11,17 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		_putchar(',');
-		_putchar(' ');
-		_putchar('\n');
 		return (1);
 	}
 	else if (n < 0)
 	{
 		_putchar('-');
-		_putchar(',');
-		_putchar(' ');
-		_putchar('\n');
-		return (-1);
+		return (1);
 	}
 	else
 	{
+		_putchar('0');
 		return (0);
 	}
+	_putchar('\n');
 }
