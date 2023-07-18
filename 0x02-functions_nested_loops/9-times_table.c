29bda3208e8ad63277be95b0c112fdbf16ff085d
Laadnani Mohamed <laadnani.med@gmail.com>
2023-07-18 08:36:46 -0700

    9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * times_table - entry point
+ *
+ * Return: time table
+ */
+void times_table(void)
+{
+	int a;
+	int b;
+	int val;
+
+	for (a = 0; a <= 9; a++)
+	{
+		for (b = 0; b <= 9; b++)
+		{
+			val = a * b;
+			_putchar(val + '0');
+			_putchar(',');
+			_putchar(' ');
+}
