ae03fc45b7ba90a0fd7d91466162c6768599e8ef
Laadnani Mohamed <laadnani.med@gmail.com>
2023-07-18 04:47:59 -0700

    1-alphabet.c

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+include "main.h"
+/**
+ * main - entry point
+ *
+ * Return: always 0
+ */
+int main(void)
+{
+	char a;
+
+	for (a = 'a'; a <= 'z'; a++)
+	{
+		_putchar(a);
+	}
+	_putchar('\n');
+}
