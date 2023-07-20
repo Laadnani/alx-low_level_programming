7715f9ed741b7dac6de9ad74ce4f17c3d31ec393
Laadnani Mohamed <laadnani.med@gmail.com>
2023-07-20 03:44:41 -0700

    crow

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -4,14 +4,13 @@
  * print_numbers - print numbers from 0 till 9
  *
  */
-
 void print_numbers(void)
 {
 	int i = 48;
 
 	while (i <= 57)
 	{
-		_putchar(i + '0');
+		_putchar(i);
 		i++;
 	}
 	_putchar('\n');
