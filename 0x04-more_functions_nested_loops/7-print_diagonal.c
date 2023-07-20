8cc5a4cd496f321f5e90bc300b9c520c0c30589b
Laadnani Mohamed <laadnani.med@gmail.com>
2023-07-20 05:20:35 -0700

    if (j == i)

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,38 @@
+#include "main.h"
+
+/**
+ * print_diagonal - print diagonal lines
+ *
+ * @n: number of diagonal pointers
+ *
+ * Return: always 0
+ */
+
+void print_diagonal(int n)
+{
+	int i;
+	int j;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j < n; j++)
+			{
+				if (j == i)
+				{
+					_putchar('\\');
+				}
+				else
+				{
+					_putchar(' ');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
