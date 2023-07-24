a635b3da5ee0c40f71424c31cabe049700b54bc8
Laadnani Mohamed <laadnani.med@gmail.com>
2023-07-24 05:49:01 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/** 
+ * print_rev - prints a string in reverse
+ *
+ * @s: string to be reversed then printed
+ */
+
+void print_rev(char *s)
+{
+	int one = 0;
+	int two;
+
+	while (*s != '\0')
+	{
+		one++;
+		s++;
+	}
+	one--;
+	for (two = one; two <= 0; two--)
+	{
+		_putchar(s[two]);
+	}
+	_putchar('\n');
+}
