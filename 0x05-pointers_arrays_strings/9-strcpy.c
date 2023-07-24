132fd9cf4254590f0385cfa43afa2ec1029d0b9a
Laadnani Mohamed <laadnani.med@gmail.com>
2023-07-24 10:36:31 -0700

    9-strcpy.c

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -10,17 +10,13 @@
 
 char *_strcpy(char *dest, char *src)
 {
-	int i =0;
-	int j;
+	int i = 0;
 
-	while (*(src + 1) != '\0')
+	do
 	{
+		dest[i] = src[i];
 		i++;
 	}
-	for (j = 0; j < i; j++)
-	{
-		dest[j] = src[j];
-	}
-	dest[i] = '\0';
+	while (src[i] != '\0');
 	return (dest);
 }	
