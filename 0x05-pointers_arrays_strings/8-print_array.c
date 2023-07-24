ed1c5d520a5c8ead88e00b369d7d4f89dede1f5e
Laadnani Mohamed <laadnani.med@gmail.com>
2023-07-24 09:01:51 -0700

    price

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - printing the elements of the array
+ *
+ * @n: number of elements of the array that will be printed
+ * Return: always 0
+ */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf('%d, ', a[i]);
+	}
+	_putchar('\n');
+}
