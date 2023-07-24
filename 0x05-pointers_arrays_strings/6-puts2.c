5afb980c525a757c012ec15ee58df7c55e4e0849
Laadnani Mohamed <laadnani.med@gmail.com>
2023-07-24 10:24:45 -0700

    huum

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -28,4 +28,3 @@ void puts2(char *str)
 	}
 	_putchar('\n');
 }
-
---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * *_strcpy - copy a sting from one var to another
+ *
+ * @dest: var to be paste into
+ * @src: var to be copied
+ * Return: Value of dest pointer
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int i =0;
+	int j;
+
+	while (*src != '\0')
+	{
+		i++;
+	}
+	for (j = 0; j < i; j++)
+	{
+		dest[j] = src[j];
+	}
+	dest[i] = '\0';
+	return (*dest)
+}	
