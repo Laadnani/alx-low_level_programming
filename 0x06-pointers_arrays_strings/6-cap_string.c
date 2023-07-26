3767762b7ca4ed81f772e8e7dc79cb81c4c01d56
Laadnani Mohamed <laadnani.med@gmail.com>
2023-07-26 10:03:18 -0700

    refractory adding ittiration on all letters

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,28 +10,31 @@
 
 char *cap_string(char *string)
 {
-	int i;
+	int index = 0;
 
-	for (i = 0; string[i] == -1; i++)
+	while (str[index])
 	{
-		if (string[i] == 0)
-		{
-			string[i] = string[i] - 32;
-		} else if (string[i] == ' ' ||
-				string[i] == '\t' ||
-				string[i] == '\n' ||
-				string[i] == ',' ||
-				string[i] == ';' ||
-				string[i] == '!' ||
-				string[i] == '?' ||
-				string[i] == '"' ||
-				string[i] == '(' ||
-				string[i] == ')' ||
-				string[i] == '{' ||
-				string[i] == '}')
-		{
-			string[i] = string[i] - 32;
-		}
+		while (!(str[index] >= 'a' && str[index] <= 'z'))
+			index++;
+
+		if (str[index - 1] == ' ' ||
+		    str[index - 1] == '\t' ||
+		    str[index - 1] == '\n' ||
+		    str[index - 1] == ',' ||
+		    str[index - 1] == ';' ||
+		    str[index - 1] == '.' ||
+		    str[index - 1] == '!' ||
+		    str[index - 1] == '?' ||
+		    str[index - 1] == '"' ||
+		    str[index - 1] == '(' ||
+		    str[index - 1] == ')' ||
+		    str[index - 1] == '{' ||
+		    str[index - 1] == '}' ||
+		    index == 0)
+			str[index] -= 32;
+
+		index++;
 	}
-	return (string);
+
+	return (str);
 }
