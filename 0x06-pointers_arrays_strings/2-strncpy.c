6ade54a4e9bdb0bd7f651060561b4136f782ee24
Laadnani Mohamed <laadnani.med@gmail.com>
2023-07-26 05:09:46 -0700

    2-strncpy.c

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * *_strncpy - copy a string over another string
+ *
+ * @n: number of char o be copied
+ * @dest: var that will hold the copied string
+ * @src: var that hold the original string
+ * Return: the copied string
+ */
+
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i;
+
+	for (i = 0; dest[i] = n; i++)
+	{
+		dest[i] = src[i];
+	}
+	return (dest);
+}
