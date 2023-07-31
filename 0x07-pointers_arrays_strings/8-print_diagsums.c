62a72d6e525b85df4161d5460c8a67f4ad3a4a06
Laadnani Mohamed <laadnani.med@gmail.com>
2023-07-31 09:53:05 -0700

    maybe my main.h file is corruptre

---
+++ 0x07-pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_diagsums - print the sums of array diaglonals
+ *
+ * @a: array to loop onto
+ * @size: size of the array
+ */
+
+void print_diagsums(int *a; int size)
+{
+	int i;
+	int first = 0;
+       	int second = 0;
+
+	for (i =0; i < size; i++)
+	{
+		first += first + a[i * size + i];
+	}
+	for (i = size - 1; i >= 0; i--)
+	{
+		second += a[i * size + (size - i - 1)]; 
+	}
+	printf("%d", first);
+	printf(", ");
+	printf("%d", second);
+}
