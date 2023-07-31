c218fcf520447fc3e6cac567774ffd5d4cf0d636
Laadnani Mohamed <laadnani.med@gmail.com>
2023-07-31 05:29:42 -0700

    adding the starting point

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * *_strchr - search for a char in a string
+ *
+ * @s: string to search in
+ * @c: char to search for
+ * Return: rest of the string if c is found or Null
+ */
+
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+	char *n = '\0';
+
+	while (s[i] != c)
+	{
+		i++;
+	}
+	if (s[i] == c)
+	{
+		return (s + i);
+	} else
+	return (n);
+}
