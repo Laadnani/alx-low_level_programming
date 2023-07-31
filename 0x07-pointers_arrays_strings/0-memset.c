888f116d34d0b27bbb046c3cf3610b5173be22b2
Laadnani Mohamed <laadnani.med@gmail.com>
2023-07-31 03:56:19 -0700

    0-memset.c

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -11,12 +11,12 @@
 
 char *_memset(char *s, char b, unsigned int n)
 {
-	int i = 0;
+	unsigned int i = 0;
 
 	while (i < n)
 	{
 		s[i] = b;
 		i++;
 	}
-	return (b)
+	return (s);
 }
---
+++ 0x07-pointers_arrays_strings/_putchar.c
@@ -0,0 +1,8 @@
+#include "main.h"
+#include "unistd"
+
+
+int _putchar(char c)
+{
+        return (write(1, &c, 1));
+}
