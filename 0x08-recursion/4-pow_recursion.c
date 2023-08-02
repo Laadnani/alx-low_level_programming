ce71ad2c7667b9ac1546d51b59f4183f2b7226e0
Laadnani Mohamed <laadnani.med@gmail.com>
2023-08-02 05:18:12 -0700

    sheeeeeesh

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - return the int x to the power y
+ *
+ * @x: first power
+ * @y: power ranger
+ * Return: x to the power y or -1 if y < 0
+ */
+
+int _pow_recursion(int x, int y)
+{
+	int temp;
+
+	if ( y < 0)
+	{
+		return (-1);
+	}
+	if ( y == 0)
+	{
+		return (1);
+	}
+	temp = x * _pow_recursion(x ,(y - 1));
+	return (temp);
+}
