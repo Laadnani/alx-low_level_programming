ae3f2b8efde57ecb48126293b3c5ab05f568671d
Laadnani Mohamed <laadnani.med@gmail.com>
2023-08-02 07:37:19 -0700

    no hands ?

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -4,28 +4,35 @@
  * is_prime_number - find s prime number
  *
  * @n: number to define as prime or not
- * Return: 1 for prime numbers otherwise 0
+ * @i: modulus divider
+ * Return: value of helper function
  */
 
-int helper_function(int x, int i);
-int is_prime_number(int x)
+int helper_function(int n, int i);
+int is_prime_number(int n)
 {
-	if (x <= 1)
+	if (n <= 1)
 		return (0);
-	else if (x == 2)
+	else if (n == 2)
 		return (1);
-	return helper_function(x, 3);
+	return (helper_function(n, 3));
 }
-
-int helper_function(int x, int i)
+/**
+ * helper_function - finds the prime number using modulus
+ *
+ * @i:number to devine n on to find modulus 0
+ * @n: given number
+ * Return: 1 if number is prime 0 otherwise
+ */
+int helper_function(int n, int i)
 {
-	if (x <= 1)
+	if (n <= 1)
 		return (0);
-	else if (x % 2 == 0)
+	else if (n % 2 == 0)
 		return (0);
-	else if (x % i == 0)
+	else if (n % i == 0)
 		return (0);
-	else if (i * i >  x)
+	else if (i * i >  n)
 		return (1);
-	return helper_function(x, i + 2);
+	return (helper_function(n, i + 2));
 }
