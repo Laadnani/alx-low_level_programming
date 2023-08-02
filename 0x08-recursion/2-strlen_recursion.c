2c22f10810ba13aa8aad69a68f5186fa42d96384
Laadnani Mohamed <laadnani.med@gmail.com>
2023-08-02 04:38:07 -0700

    recursion can be on the arg too

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - return the length of the string
+ *
+ * @s: string the mesure
+ * Return: string length
+ */
+
+int _strlen_recursion(char *s)
+{
+	int len = 0;
+
+	if (*s > '\0')
+	{
+		len += _strlen_recursion(s + 1) + 1;
+	} 
+	return (len);
+}
