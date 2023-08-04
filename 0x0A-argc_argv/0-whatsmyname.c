59c026b405de94c3734ef743c810a9ddd575c48d
Laadnani Mohamed <laadnani.med@gmail.com>
2023-08-04 10:30:08 -0700

    man

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -3,9 +3,13 @@
 
 /**
  * main - prints the file/program file
+ *
+ * @argc: arguments count
+ * @argv: arguments vector
+ * Return: always 0
  */
 
-int main(int argc,char *argv[])
+int main(int argc, char *argv[])
 {
 	printf("%s\n", argv[argc - 1]);
 	return (0);
