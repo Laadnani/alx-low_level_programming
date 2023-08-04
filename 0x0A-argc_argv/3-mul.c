a4ed2c45e9ca96e243902af16954e717e9e20ec4
Laadnani Mohamed <laadnani.med@gmail.com>
2023-08-04 12:43:47 -0700

    inside

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -11,16 +11,17 @@
  */
 int main(int argc, char *argv[])
 {
-	int  sum;
-	int a = atoi(argv[1]);
-	int b = atoi(argv[2]);
-
-	if (argc == 3)
+	if (argc != 3)
 	{
+		printf("Error\n");
+		return (1);
+	} else {
+		int  sum;
+        	int a = atoi(argv[1]);
+       		int b = atoi(argv[2]);
+
 		sum = a * b;
 		printf("%d\n", sum);
 		return (0);
 	}
-	printf("Error\n");
-	return (1);
 }
