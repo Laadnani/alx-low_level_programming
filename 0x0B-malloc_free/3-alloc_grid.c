50a000fcf91ca70ec287270fc7eb7555ec1e9838
Laadnani Mohamed <laadnani.med@gmail.com>
2023-08-07 04:52:40 -0700

    allowed or no ?

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -25,6 +25,15 @@ int **alloc_grid(int width, int height)
 		for (i = 0; i < height; i++)
 		{
 			grid[i] = malloc(sizeof(**grid) * width);
+			if(grid[i] == 0)
+			{
+				while (i--)
+				{
+					free(grid[i]);
+				}
+				free(grid);
+				return (NULL);
+			}
 				for (j = 0; j < width; j++)
 				{
 					grid[i][j] = 0;
