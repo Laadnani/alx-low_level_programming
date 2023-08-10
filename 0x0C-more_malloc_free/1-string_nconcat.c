4420588f4af2a49ddeb8e50b85af841e35f4e175
Laadnani Mohamed <laadnani.med@gmail.com>
2023-08-10 03:38:32 -0700

    huum

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,51 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *string_nconcat - concat 2 string till n bite of s2
+ * 
+ * @s1: first string to concat
+ * @s2: second string to concat
+ * @n: number of bytes to concat from s2
+ * Return: concat strings s1 and s2
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i = 0;
+	unsigned int j = 0;
+	int ik, jk;
+	char *c;
+       
+	for (ik = 0; s1[ik] != '\0'; ik++);
+	for (jk = 0; s2[jk] != '\0'; jk++);
+	c = malloc(sizeof(char)*(ik + jk));
+
+	if (c == NULL)
+	{
+		return (NULL);
+	}
+	for (; s1[i] != '\0'; i++)
+	{
+		if (s1 == NULL)
+		{
+			break;
+		}
+		else
+		{
+			c[i] = s1[i];
+		}
+	}
+	for (; s2[j] != '\0' && j <= n; j++)
+	{
+		if (s2 == NULL)
+		{
+			break;
+		}
+		else 
+		{
+			c[i + j] = s2[j];
+		}
+	}
+	return (c);
+}
