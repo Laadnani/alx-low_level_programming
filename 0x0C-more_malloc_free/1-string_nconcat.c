655076aa175c3e91cdbeb1b387864c3386baf41b
Laadnani Mohamed <laadnani.med@gmail.com>
2023-08-10 03:55:02 -0700

    how is it

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -37,11 +37,11 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 	for (; s1[i] != '\0'; i++)
 	{
-			c[i] = s1[i];
+		c[i] = s1[i];
 	}
 	for (; s2[j] != '\0' && j <= n; j++)
 	{
-			c[i + j] = s2[j];
+		c[i + j] = s2[j];
 	}
 	c[i + j] = '\0';
 	return (c);
