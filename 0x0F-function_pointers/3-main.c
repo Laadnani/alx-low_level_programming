19b7a0cb187800a001b21ac11908033e035f0ddc
Laadnani Mohamed <laadnani.med@gmail.com>
2023-08-16 08:18:05 -0700

    maybe

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -16,20 +16,20 @@ int main(int argc, char *argv[])
 
 	if (argc != 4)
 	{
-		printf("Error \n");
+		printf("Error\n");
 		return (98);
 	}
 	op_func = get_op_func(argv[2]);
 	if (!op_func)
 	{
-		printf("Error \n");
+		printf("Error\n");
 		return (99);
 	}
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 	if (!b && ((argv[2][0] == '/') || (argv[2][0] == '%')))
 	{
-		printf("Error \n");
+		printf("Error\n");
 		return (100);
 	}
 	printf("%d\n", op_func(a, b));
