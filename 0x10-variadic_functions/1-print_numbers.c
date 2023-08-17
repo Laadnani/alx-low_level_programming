7d9ae21b2e36878743c8aafa55c8443fbbbb7359
Laadnani Mohamed <laadnani.med@gmail.com>
2023-08-17 05:16:14 -0700

    collins

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -21,6 +21,7 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 			if (i == n - 1)
 			{
 				printf("%d\n", va_arg(args, unsigned int));
+				break;
 			}
 			if (!separator || *separator == 0)
 			{
