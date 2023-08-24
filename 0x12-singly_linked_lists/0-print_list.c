c76cfcf1d162b81077a39f6b19d688d46ede2e22
Laadnani Mohamed <laadnani.med@gmail.com>
2023-08-24 08:33:41 -0700

    alloc

---
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -0,0 +1,26 @@
+#include "lists.h"
+
+/**
+ * print_list - print a list of node params
+ * @h: node to work on
+ * Return: number of nodes worked on
+ */
+
+size_t print_list(const list_t *h)
+{
+	unsigned int count = 0;
+	const list_t *current = h;
+
+	while (current != NULL)
+	{
+		if (current->str == NULL)
+			printf("[0] (nil)\n");
+		else
+		{
+			printf("[%i] %s\n", current->len, current->str);
+		}
+		current = current->next;
+		count++;
+	}
+	return (count);
+}
