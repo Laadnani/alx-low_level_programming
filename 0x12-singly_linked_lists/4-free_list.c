5489ef25c149549592fc6367e81356bfd331e044
Laadnani Mohamed <laadnani.med@gmail.com>
2023-08-24 10:16:28 -0700

    still getting in

---
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * free-list - free the memory of all the nodes
+ * @head: node address pointer
+ */
+
+void free_list(list_t *head)
+{
+	list_t *current = head;
+	list_t *next;
+
+	while (current->next)
+	{
+		next = current->next;
+		free(current->str);
+		free(current);
+		current = next;
+	}
+}
