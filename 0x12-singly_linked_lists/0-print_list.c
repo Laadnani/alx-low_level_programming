802cadb72e5125c2bdd05393ecb1d5750b683f75
Laadnani Mohamed <laadnani.med@gmail.com>
2023-08-24 10:39:42 -0700

    lets see

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -14,7 +14,7 @@ size_t print_list(const list_t *h)
 	while (current != NULL)
 	{
 		if (current->str == NULL)
-			printf("[0] (nil)\n");
+			printf("[%i] (nil)\n", current->len);
 		else
 		{
 			printf("[%i] %s\n", current->len, current->str);
