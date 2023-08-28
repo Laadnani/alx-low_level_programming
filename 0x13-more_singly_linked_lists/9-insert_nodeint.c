1a631e827e192df2848973beac7cc479a32f523b
Laadnani Mohamed <laadnani.med@gmail.com>
2023-08-28 08:50:02 -0700

    huum

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -25,7 +25,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	}
 	while (i < idx - 1)
 	{
-		if (current->next == NULL && i < idx)
+		if (current->next == NULL)
 		{
 			return (NULL);
 		}
