296cc02a329b00198900306ebe8d151fc7c07344
Laadnani Mohamed <laadnani.med@gmail.com>
2023-08-28 08:56:48 -0700

    lets check

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,13 +19,16 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	{
 		return (NULL);
 	}
-	if (*head == NULL)
+	if (idx == 0)
 	{
-		return (NULL);
+		new_node->n = n;
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
 	}
 	while (i < idx - 1)
 	{
-		if (current->next == NULL)
+		if (current == NULL)
 		{
 			return (NULL);
 		}
