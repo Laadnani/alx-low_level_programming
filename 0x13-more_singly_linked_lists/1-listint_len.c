1632c00aa329c10938307dbbae8ba4094e53eefd
Laadnani Mohamed <laadnani.med@gmail.com>
2023-08-28 00:39:38 -0700

    mom

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * listint_len - get he number of nodes
+ * @h: head of the linked lists pointer
+ * Return: number of nodes
+ */
+
+size_t listint_len(const listint_t *h)
+{
+	unsigned int count = 0;
+	const listint_t *current = h;
+
+	while(current != NULL)
+	{
+		current = current->next;
+		count++;
+	}
+	return (count);
+}
---
+++ 0x13-more_singly_linked_lists/2-add_nodeint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdlib.h>
+
+/**
+ * add_nodeint - add a new node to the beginning of a list
+ * @head: pointer to the pointer of the head
+ * Return: address of the new node or Null
+ */
+
+listint_t *add_nodeint(listint_t **head, const int n)
+{
+	listint_t *new_node;
+	new_node = (listint_t *) malloc(sizeof(listint_t));
+
+	if (!new_node)
+	{
+		return(NULL);
+	}
+	new_node->n = n;
+	new_node->next = *head;
+	*head = new_node;
+	return (new_node);
+}
