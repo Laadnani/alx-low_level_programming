c8c29d319f346534b59364bc42e9d3dc75a1fb8a
Laadnani Mohamed <laadnani.med@gmail.com>
2023-08-28 08:14:03 -0700

    an

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -11,7 +11,7 @@ size_t listint_len(const listint_t *h)
 	unsigned int count = 0;
 	const listint_t *current = h;
 
-	while(current != NULL)
+	while (current != NULL)
 	{
 		current = current->next;
 		count++;
