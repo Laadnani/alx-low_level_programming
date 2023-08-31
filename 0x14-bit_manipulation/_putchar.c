044dccf3f5805b80f8b9fac90e7b2e2cf0f81041
Laadnani Mohamed <laadnani.med@gmail.com>
2023-08-31 03:51:29 -0700

    0.0

---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - prints chars
+ * @c: char to print
+ * Return: printed char
+ */
+
+int _putchar(char *s)
+{
+	return(wrtie(1, &c, 1));
+}
