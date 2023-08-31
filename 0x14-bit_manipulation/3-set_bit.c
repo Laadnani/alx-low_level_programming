78d217cb09591674fe365d9c18a080a18a244db7
Laadnani Mohamed <laadnani.med@gmail.com>
2023-08-31 07:47:26 -0700

    check

---
+++ 0x14-bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * set_bit - set a bit 1 to a giver number on given index
+ * @n: number given
+ * @index: index to set 1 into
+ * Return: 1 on success or -1 otherwise
+ */
+int set_bit(unsigned long int *n, unsigned int index)
+{
+	unsigned long int i = 1;
+	unsigned long int bin = 0; 
+	unsigned long int rem = 1;
+
+	while (n != 0)
+	{
+
+	}
+}
