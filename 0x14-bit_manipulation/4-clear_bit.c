bd7462de87710cdec79e8485f5acfe3639b0a06d
Laadnani Mohamed <laadnani.med@gmail.com>
2023-08-31 09:56:03 -0700

    cccc

--- 0x14-bit_manipulation/4-clear_bit.c
+++ 0x14-bit_manipulation/4-clear_bit.c
@@ -1,17 +1,17 @@
 #include "main.h"
 
 /**
- * set_bit - set a bit 0 to a giver number on given index
+ * clear_bit - set a bit 0 to a giver number on given index
  * @n: number given
  * @index: index to set 0 into
  * Return: 1 on success or -1 otherwise
  */
-int set_bit(unsigned long int *n, unsigned int index)
+int clear_bit(unsigned long int *n, unsigned int index)
 {
-	if (index >= sizeof(n) * 8);
+	if (index >= sizeof(n) * 8)
 	{
 		return (-1);
 	}	
-	*n |= ~(1UL << index);
+	*n &= ~(1UL << index);
 	return (1);
 }
