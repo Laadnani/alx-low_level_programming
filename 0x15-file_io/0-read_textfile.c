f7d752cc82d0c1e3f76dc0a6be3d0413b7d186ea
Laadnani Mohamed <laadnani.med@gmail.com>
2023-09-04 09:18:38 -0700

    man page

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * read_textfile - read text file and print it back
+ * @filename: file to read
+ * @letters: number of chars to print
+ * Return: number of chars written
+ */
+
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fd;
+	char *buf;
+	size_t bytes_read, bytes_written;
+
+	fd = open(filename, O_RDONLY);
+	if (fd == -1 || filename == NULL)
+	{
+		return (0);
+	}
+	buf = malloc(letters);
+	if (buf == NULL)
+	{
+		close(fd);
+		return (0);
+	}
+	bytes_read = read(fd, buf, letters);
+	if (!bytes_read)
+	{
+		close(fd);
+		free(buf);
+		return (0);
+	}
+	bytes_written = write(STDOUT_FILENO, buf, bytes_read);
+	if (!bytes_written)
+	{
+		close(fd);
+		free(buf);
+		return (0);
+	}
+	return (bytes_written);
+}
---
+++ 0x15-file_io/README.md
@@ -0,0 +1,1 @@
+0x15. C - File I/O eplainaition on the upcoming post on medium.com/Laadnani
