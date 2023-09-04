1d0d970757aa15932ce7eebe3015b3cb899db4e1
Laadnani Mohamed <laadnani.med@gmail.com>
2023-09-04 13:18:03 -0700

    now ?

---
+++ 0x15-file_io/3-cp.c
@@ -0,0 +1,63 @@
+#include "main.h"
+
+#define BUFFER 1024
+
+/**
+ * cpi - copy the content of a file src into a file dest
+ * @argc: file name  - src file - dest file
+ * @argv: arguments vector
+ * Return: 1 upon success or exit codes other wise
+ */
+
+int main(int argc, char *argv[])
+{
+	int src_fd, dest_fd;
+	char buf[BUFFER];
+	ssize_t bytes_r, bytes_w;
+
+	if (argc != 3)
+	{
+		dprintf(STDERR_FILENO,"Usage: cp file_from file_to\n");
+		exit(97);
+	}
+	src_fd = open(argv[1], O_RDONLY);
+	if (src_fd == -1)
+	{
+		dprintf(STDERR_FILENO,"Error: can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	dest_fd = open(argv[2],O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	if (dest_fd == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: can't write to file %s\n", argv[2]);
+		exit(99);
+	}
+	bytes_r = read(src_fd, buf, BUFFER);
+	if (bytes_r == -1)
+	{
+		dprintf(STDERR_FILENO,"Error: can't read from file %s\n", argv[1]);
+		exit(98);
+	}
+	while(bytes_r > 0)
+	{
+		bytes_w = write(dest_fd, buf, bytes_r);
+		if (bytes_w == -1)
+		{
+			dprintf(STDERR_FILENO, "Error: can't wrtie to file %s\n", argv[2]);
+			exit(99);
+		}
+	}
+	close(src_fd);
+	if (close(src_fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", src_fd);
+		exit(100);
+	}
+	close(dest_fd);
+	if (close(dest_fd) == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", dest_fd);
+		exit(100);
+	}
+	return (0);
+}
--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -4,9 +4,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <fcntl.h>
+
 int create_file(const char *filename, char *text_content);
 ssize_t _strln(const char *str);
 int append_text_to_file(const char *filename, char *text_content);
+int cpi(int argc, char *argv[]);
 int _putchar(char s);
 ssize_t read_textfile(const char *filename, size_t letters);
 
