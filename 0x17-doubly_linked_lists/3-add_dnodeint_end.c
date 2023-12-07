47c83f9ac2bb31496aa134262b1368cadb258d8c
Laadnani mohamed <laadnani.med@gmail.com>
2023-12-07 05:11:43 -0800

    spaces

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ * add_dnodeint_end - add a new node to the end of the DLL
+ * @head: the new node head
+ * @n: data of the node
+ * Return: address of the new node or Null in unsuccesfull
+ */
+
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new;
+
+	new = (dlistint_t *) malloc(sizeof(dlistint_t));
+	if (!new)
+	{
+		free(new);
+		return (NULL);
+	}
+	new->n = n;
+	new->next = *head;
+	*head = new;
+	return (new);
+}
