591b05f00e7e494d7ff5fd6302f7b72d090e27f8
Laadnani mohamed <laadnani.med@gmail.com>
2023-12-07 07:45:03 -0800

    asd

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -17,12 +17,17 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	}
 	new->n = n;
-	new->next = *head;
+	new->prev = NULL;
 	if (*head != NULL)
 	{
-		(*head)->prev = new;
+		*head = new;
+		new->next = NULL;
 	}
-	new->prev = new;
-	*head = new;
+	else
+	{
+		new->next = *head;
+		(*head)->prev = new;
+		*head = new;
+	}
 	return (new);
 }
